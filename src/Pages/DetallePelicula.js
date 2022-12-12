6f945e57795b39752b90671eeb2c9fc861bf1160
Florencia <florencianoelcarrillo@gmail.com>
2022-12-12 16:10:57 -0300

    se crea funcionalidad para descargar detalle

--- src/Pages/DetallePelicula.js
+++ src/Pages/DetallePelicula.js
@@ -4,13 +4,14 @@ import { obtenerPosterPelicula } from '../Services/obtenerPosterPelicula';
 import styles from "../Styles/DetallePelicula.module.css";
 import { get } from '../Services/httpClient';
 import { Loader } from '../Components/Loader';
+import html2canvas from 'html2canvas';
 
 export function DetallePelicula() {
     const { idPelicula } = useParams();
-    const [ cargando, setCargando ] = useState(true);
-    const [ pelicula, setPelicula ] = useState(null);
+    const [cargando, setCargando] = useState(true);
+    const [pelicula, setPelicula] = useState(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         setCargando(true);
         get("/movie/" + idPelicula).then(datos => {
             setPelicula(datos);
@@ -18,28 +19,42 @@ export function DetallePelicula() {
         })
     }, [idPelicula]);
 
-    if(cargando){
+    if (cargando) {
         return <Loader />
     }
 
-    if(!pelicula){
+    if (!pelicula) {
         return null;
     }
 
-    
+
+    const descargarInfo = () => {
+        html2canvas(document.getElementById("infoDescargar"), { backgroundColor: "#333333", allowTaint: false, useCORS: true }).then(function (canvas) {
+            const link = document.createElement("a");
+            link.href = canvas.toDataURL();
+            link.download = "InfoPelicula/jpg"
+            link.click();
+        });
+    }
+
+
     const imgUrl = obtenerPosterPelicula(pelicula.poster_path, 500);
-    return(
+    return (
         <div className={styles.contenedor}>
-            <img 
-            src={imgUrl} 
-            alt="Imagen de la pelicula"
-            className={`${styles.columna} ${styles.img}`}
-            />
-            <div className={`${styles.columna} ${styles.descripcion}`}>
-                <p><strong>Titulo: </strong>{pelicula.title}</p>
-                <p><strong>Género: </strong>{pelicula.genres.map(genero => genero.name).join(", ")}</p>
-                <p><strong>Descripción: </strong>{pelicula.overview}</p>
+            <div className={styles.contenedor} id="infoDescargar">
+                <img
+                    src={imgUrl}
+                    alt="Imagen de la pelicula"
+                    className={`${styles.columna} ${styles.img}`}
+                />
+                <div className={`${styles.columna} ${styles.descripcion}`}>
+                    <p><strong>Titulo: </strong>{pelicula.title}</p>
+                    <p><strong>Género: </strong>{pelicula.genres.map(genero => genero.name).join(", ")}</p>
+                    <p><strong>Descripción: </strong>{pelicula.overview}</p>
+                </div>
             </div>
+            <button onClick={descargarInfo}>Descargar Info</button>
         </div>
+
     );
 }
