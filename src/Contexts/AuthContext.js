471f7aa24791ab4e8f2115d4321da2042ae13537
leagiardili <lgiardili@gmail.com>
2022-12-15 18:25:48 -0300

    'addFavorito'

--- src/Contexts/AuthContext.js
+++ src/Contexts/AuthContext.js
@@ -49,3 +49,5 @@ export function AuthProvider({ children }) {
 
   return <AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>;
 }
+
+
--- src/Pages/AddUser.js
+++ src/Pages/AddUser.js
@@ -1,6 +1,12 @@
 import Firebase,{ db } from '../firebase';
 import {collection, getDocs, getDoc, query, doc, addDoc, deleteDoc, updateDoc} from 'firebase/firestore';
 
+
 export const addUser=(usrEmail,usrPassword) =>{
     addDoc(collection(db,'usuarios'),{usrEmail,usrPassword});
 }
+
+export const addFavorito=(userId, peliId) =>{
+    return addDoc(collection(db,'favoritos'),{userId,peliId});
+}
+
